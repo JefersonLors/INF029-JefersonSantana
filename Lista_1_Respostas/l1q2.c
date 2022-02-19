567f8397330f00a0584f4f0eaeea70f35589f5c6
Jeferson Lors <69722947+JefersonLors@users.noreply.github.com>
2022-02-19 15:07:56 +0000

    deixando o 0 com sinal

--- Lista_1_Respostas/l1q2.c
+++ Lista_1_Respostas/l1q2.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 
 int main( ){
-  int soma(int valor_1, int valor_2 );
+  int subtrai(int valor_1, int valor_2 , int valor_3);
   
   int valor_1 = 0,
-      valor_2 = 0;
+      valor_2 = 0,
+      valor_3 = 0;
       
   enum CONTINUA { NAO = 0, SIM = 1 };
   
@@ -13,16 +14,18 @@ int main( ){
 
   puts("\n");
   do{
-    printf( "-> Digite dois valores para saber sua soma\n");
+    printf( "-> Digite tres valores para saber sua diferenca\n");
     printf( "   > valor 1 = " );
     scanf( "%d", &valor_1 );
     printf( "   > valor 2 = " );
     scanf( "%d", &valor_2 );
+    printf( "   > valor 3 = " );
+    scanf( "%d", &valor_3 );
 
-    printf("\n   > ( %+d ) + ( %+d ) = %+d \n", valor_1, valor_2, soma( valor_1, valor_2 ) );
-    
+    printf("\n   > ( %+d ) - ( %+d ) - ( %+d ) = %+d \n", valor_1, valor_2, valor_3, subtrai( valor_1, valor_2, valor_3 ) );
+
     do{
-      printf( "\n-> Deseja somar de novo?\n"
+      printf( "\n-> Deseja subtrair de novo?\n"
               "   [1] SIM       [0] NAO\n\n   > " );
       scanf( "%d", &resposta );
     }while( resposta != NAO && resposta != SIM );
@@ -34,6 +37,6 @@ int main( ){
   }while( resposta == SIM );
   return 0;
 }
-int soma( int v_1, int v_2 ){
-  return v_1 + v_2;
+int subtrai( int v_1, int v_2, int v_3 ){
+  return v_1 - v_2 - v_3;
 }
