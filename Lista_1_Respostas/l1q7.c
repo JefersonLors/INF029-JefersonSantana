42ab761f76dc616dbace68ce9fb3c2fa9119a53a
Jeferson Lors <69722947+JefersonLors@users.noreply.github.com>
2022-02-21 01:37:30 +0000

    devolvendo três palavras

---
+++ Lista_1_Respostas/l1q7.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define quantidade 3
+
+struct palavras{
+    char palavra[50];
+};
+
+typedef struct palavras Palavras;
+
+int main( ){
+  Palavras *ler3palavras( void );
+
+  enum CONTINUA { NAO = 0, SIM = 1 };
+  
+  int resposta = SIM;
+  
+  Palavras *palavras = NULL;
+
+  puts("\n");
+  do{
+    printf( "-> Digite três palavras\n");
+    palavras = ler3palavras( );
+    
+    printf( "\n-> As palavras foram\n");
+    for( int local = 0; local < quantidade; local++ ){
+      printf( "   > palavra [%d] = %s\n", local, palavras[local].palavra );}
+    
+    do{
+      printf( "\n-> Deseja armazenar mais três palavras?\n"
+              "       [1] SIM               [0] NAO\n\n   > " );
+      scanf( "%d", &resposta );
+    }while( resposta != NAO && resposta != SIM );
+
+    if( resposta == NAO ){
+      printf( "\n          <<< PROGRAMA ENCERRADO >>>" );}
+
+    puts("\n");
+  }while( resposta == SIM );
+  return 0;
+}
+
+Palavras *ler3palavras( ){
+  static Palavras armazem[quantidade];
+
+  for( int local = 0; local < quantidade; local++ ){
+    printf( "   > palavra [%d] = ", local );
+    scanf( "%s", armazem[local].palavra );}
+  return armazem;
+} 
